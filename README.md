2609ea9152491bf224af2c0acc2a2ebe1f9da07c
leesaewa <saewa0424@gmail.com>
2022-10-11 16:56:13 +0900

    221011: added js

--- README.md
+++ README.md
@@ -4,7 +4,17 @@
 
 ## log
 
+### 221011
+
+#### Javascript, HTML, CSS
+
+- `header` -> `hamburger menu` 구축
+- `header` -> 스크롤 이벤트 구축
+- `fade in` animation 구축
+
 ### 221007
+
+#### Javascript, HTML, CSS
 
 - 작업 시작
 - `cursor`, `header`, `footer` 구축
--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,93 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // cursor
-  let cursor = document.querySelectorAll(".cursor");
-  let cursorFirst = document.querySelector(".cursor__first");
-  let cursorSecond = document.querySelector(".cursor__second");
-  let linkCursor = document.querySelectorAll("a");
+const body = document.querySelector("body");
+let winH = window.innerHeight;
 
-  cursor.forEach((cursor) => {
-    window.addEventListener("mousemove", (e) => {
-      cursor.style.left = `${e.pageX}px`;
-      cursor.style.top = `${e.pageY}px`;
-    });
+// cursor
+const cursor = document.querySelectorAll(".cursor");
+const cursorFirst = document.querySelector(".cursor__first");
+const cursorSecond = document.querySelector(".cursor__second");
+const linkCursor = document.querySelectorAll("a");
+
+cursor.forEach((cursor) => {
+  window.addEventListener("mousemove", (e) => {
+    cursor.style.left = `${e.pageX}px`;
+    cursor.style.top = `${e.pageY}px`;
   });
-
-  linkCursor.forEach((link) => {
-    link.addEventListener("mouseover", () => {
-      cursorFirst.classList.add("cursor__link");
-      cursorSecond.classList.add("cursor__link");
-    });
-
-    link.addEventListener("mouseleave", () => {
-      cursorFirst.classList.remove("cursor__link");
-      cursorSecond.classList.remove("cursor__link");
-    });
-  });
-
-  // global nav
-  const navBtn = document.querySelector(".hamburger-btn");
-  const globalnav = document.querySelector(".global-nav");
-
-  navBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    if (!globalnav.classList.contains("global-nav--open")) {
-      globalnav.classList.add("global-nav--open");
-      navBtn.classList.add("hamburger-btn-open");
-    } else {
-      globalnav.classList.remove("global-nav--open");
-      navBtn.classList.remove("hamburger-btn-open");
-    }
-  });
-
-  //
 });
+
+linkCursor.forEach((link) => {
+  link.addEventListener("mouseover", () => {
+    cursorFirst.classList.add("cursor__link");
+    cursorSecond.classList.add("cursor__link");
+  });
+
+  link.addEventListener("mouseleave", () => {
+    cursorFirst.classList.remove("cursor__link");
+    cursorSecond.classList.remove("cursor__link");
+  });
+});
+
+// scroll header fixed
+const header = document.querySelector("header");
+const headerHeight = header.offsetHeight;
+
+// window가 스크롤 될 때 발생
+window.onscroll = () => {
+  let scrollY = window.scrollY;
+  // header의 사이즈보다 스크롤한 값이 크거나 같을 때 실행
+  if (scrollY >= headerHeight) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+};
+
+// global nav
+const navBtn = document.querySelector(".hamburger-btn");
+const globalnav = document.querySelector(".global-nav");
+
+navBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (!globalnav.classList.contains("global-nav--open")) {
+    globalnav.classList.add("global-nav--open");
+    navBtn.classList.add("hamburger-btn-open");
+    body.style.overflowY = "hidden";
+  } else {
+    globalnav.classList.remove("global-nav--open");
+    navBtn.classList.remove("hamburger-btn-open");
+    body.style.overflowY = "auto";
+  }
+});
+
+//
+// animation effects
+//
+
+const animationEff = () => {
+  let effects;
+
+  const initAni = () => {
+    effects = document.querySelectorAll(".eff");
+    eventHandler();
+  };
+
+  const eventHandler = () => {
+    window.addEventListener("scroll", checkScroll);
+    window.addEventListener("load", checkScroll);
+    window.addEventListener("resize", initAni);
+  };
+
+  const checkScroll = () => {
+    for (let i = 0; i < effects.length; i++) {
+      // viewTop이 스크롤에 의해 유동적으로 변함.
+      let viewTop = effects[i].getBoundingClientRect().top;
+      // console.log(viewTop);
+      if (winH > viewTop) {
+        effects[i].classList.add("fadeIn");
+      }
+    }
+  };
+
+  return initAni();
+};
+animationEff();
